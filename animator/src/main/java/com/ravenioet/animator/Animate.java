9de6fa4aa4060cdad358ffde1f28cbc6650b4c4e
walelgn <walelgnlinux@gmail.com>
2021-09-07 15:12:52 +0300

    duration of animation changed to manual as user requeted

--- animator/src/main/java/com/ravenioet/animator/Animate.java
+++ animator/src/main/java/com/ravenioet/animator/Animate.java
@@ -1,98 +1,170 @@
 package com.ravenioet.animator;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.DialogFragment;
 
 public class Animate extends DialogFragment {
-    static public Animation inFromRightAnimation() {
+    static public Animation inFromRightAnimation(int duration) {
         Animation inFromRight = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromRight.setDuration(500);
+        inFromRight.setDuration(duration);
         inFromRight.setInterpolator(new AccelerateInterpolator());
         return inFromRight;
     }
-
-    static public Animation outToLeftAnimation() {
+    static public Animation outToLeftAnimation(int duration) {
         Animation outtoLeft = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, -1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        outtoLeft.setDuration(500);
+        outtoLeft.setDuration(duration);
         outtoLeft.setInterpolator(new AccelerateInterpolator());
         return outtoLeft;
     }
 
-    static public Animation inFromLeftAnimation() {
+    static public Animation inFromLeftAnimation(int duration) {
         Animation inFromLeft = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, -1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromLeft.setDuration(500);
+        inFromLeft.setDuration(duration);
         inFromLeft.setInterpolator(new AccelerateInterpolator());
         return inFromLeft;
     }
 
-    static public Animation outToRightAnimation() {
+    static public Animation outToRightAnimation(int duration) {
         Animation outtoRight = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        outtoRight.setDuration(500);
+        outtoRight.setDuration(duration);
         outtoRight.setInterpolator(new AccelerateInterpolator());
         return outtoRight;
     }
-
     //vertical
-    static public Animation outToTopAnimation() {
+    static public Animation outToTopAnimation(int duration) {
         Animation outToTop = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        outToTop.setDuration(500);
+        outToTop.setDuration(duration);
         outToTop.setInterpolator(new AccelerateInterpolator());
         return outToTop;
     }
-
-    static public Animation outToBottomAnimation() {
+    static public Animation outToBottomAnimation(int duration) {
         Animation outToBottom = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        outToBottom.setDuration(500);
+        outToBottom.setDuration(duration);
         outToBottom.setInterpolator(new AccelerateInterpolator());
         return outToBottom;
     }
-
-    static public Animation inFromBottomAnimation() {
+    static public Animation inFromBottomAnimation(int duration) {
         Animation inFromBottom = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromBottom.setDuration(500);
+        inFromBottom.setDuration(duration);
         inFromBottom.setInterpolator(new AccelerateInterpolator());
         return inFromBottom;
     }
-
-    static public Animation inFromTopAnimation() {
+    static public Animation inFromTopAnimation(int duration) {
         Animation inFromTop = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, +1.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromTop.setDuration(500);
+        inFromTop.setDuration(duration);
         inFromTop.setInterpolator(new AccelerateInterpolator());
         return inFromTop;
+    }
+
+    static public Animation fadeIn(int duration) {
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(duration);
+        return fadeIn;
+    }
+    static public Animation fadeOut(int duration) {
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(duration);
+        fadeOut.setDuration(1000);
+        return fadeOut;
+    }
+
+    public static void expand(final View view) {
+        view.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = view.getMeasuredHeight();
+
+        // Set initial height to 0 and show the view
+        view.getLayoutParams().height = 0;
+        view.setVisibility(View.VISIBLE);
+
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredHeight(), targetHeight);
+        anim.setInterpolator(new AccelerateInterpolator());
+        anim.setDuration(500);
+        anim.addUpdateListener(animation -> {
+            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+            layoutParams.height = (int) (targetHeight * animation.getAnimatedFraction());
+            view.setLayoutParams(layoutParams);
+        });
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // At the end of animation, set the height to wrap content
+                // This fix is for long views that are not shown on screen
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            }
+        });
+        anim.start();
+    }
+    public static void collapse(final View view) {
+        view.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = 70/*view.getMeasuredHeight()*/;
+
+        // Set initial height to 0 and show the view
+        view.getLayoutParams().height = 0;
+        view.setVisibility(View.VISIBLE);
+
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredHeight(), targetHeight);
+        anim.setInterpolator(new AccelerateInterpolator());
+        anim.setDuration(100);
+        anim.addUpdateListener(animation -> {
+            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+            layoutParams.height = (int) (targetHeight * animation.getAnimatedFraction());
+            view.setLayoutParams(layoutParams);
+        });
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // At the end of animation, set the height to wrap content
+                // This fix is for long views that are not shown on screen
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            }
+        });
+        anim.start();
     }
 }
